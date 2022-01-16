b4c0348e4de25de43296498953230fbe185560b0
utkarsh seth <utkarshseth2019@gmail.com>
2022-01-16 11:25:29 +0530

    make dark mode of map

--- pages/api/api.js
+++ pages/api/api.js
@@ -2,22 +2,22 @@ const path = "https://restcountries.com/v3.1";
 // const path = "https://restcountries.com/v2";
 // const path = "https://restcountries.com/v3.1/name/peru";
 
-export const apiData = async() => {
-    const res = await fetch(`${path}/all`);
-    // const res = await fetch(`https://restcountries.com/v3.1/all`);
-    const responseJson = await res.json();
-    return { allData: responseJson };
-};
+// export const apiData = async() => {
+//     const res = await fetch(`${path}/all`);
+//     const responseJson = await res.json();
+//     return { allData: responseJson };
+// };
 
-export const getCountryDataByName = async name => {
-    const res = await fetch(`${path}/name/${name}`);
-    const responseJson = await res.json();
-    return responseJson;
-};
-export const getCountryLocation = async name => {
-    const res = await fetch(pathLatLong);
-    const responseJson = await res.json();
-    return { allData: responseJson };
-};
+// export const getCountryDataByName = async name => {
+//     const res = await fetch(`${path}/name/${name}`);
+//     const responseJson = await res.json();
+//     return responseJson;
+// };
+// export const getCountryLocation = async name => {
+//     const res = await fetch(pathLatLong);
+//     const responseJson = await res.json();
+//     return { allData: responseJson };
+// };
 
-//
+//  api for to  get Map latitude and longitude
+// https://api.mapbox.com/geocoding/v5/mapbox.places/delhi.json?limit=2&access_token=pk.eyJ1IjoidXRrYXJzaHNldGgiLCJhIjoiY2t5Y2VxaHJ0MG9weTJvbjBmYXR5a2VwMiJ9.GqGwztQQ9gHMrcivHTyTcw
